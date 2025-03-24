081778e5cc8b952463ffbbac6b5f97f681a47079
TomBombadiI <78550692+TomBombadiI@users.noreply.github.com>
2025-03-24 16:35:08 +0700

    add: gift modal

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -195,3 +195,24 @@ ytModalOpenButtons.forEach(element => {
 });
 
 const articleModal = new Modal('#article-modal');
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+function setCookie(name, value, days) {
+    const expires = new Date(Date.now() + days * 864e5).toUTCString();
+    document.cookie = `${name}=${value}; expires=${expires}; path=/`;
+}
+
+const isVisited = getCookie('visited');
+
+const giftModal = new Modal('#gift-modal');
+if (!isVisited) {
+    giftModal.open();
+
+    // Устанавливаем куку на 365 дней
+    setCookie('visited', 'true', 7);
+}
